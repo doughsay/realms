f35f72b6bf61cf7db4a275df9504ddae1fd54c92
Chris Dosé <chris@xn--dos-dma.com>
2026-01-30 18:09:36 -0800

    Fix formatting

--- priv/repo/migrations/20260131011447_create_players.exs
+++ priv/repo/migrations/20260131011447_create_players.exs
@@ -5,8 +5,10 @@ defmodule Realms.Repo.Migrations.CreatePlayers do
     create table(:players, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :name, :text, null: false
+
       add :current_room_id, references(:rooms, type: :binary_id, on_delete: :restrict),
-          null: false
+        null: false
+
       add :last_seen_at, :utc_datetime, null: false
 
       timestamps(type: :utc_datetime)
